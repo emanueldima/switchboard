891222ff0623115ad966c1a7d71318f66228802c
Emanuel Dima <emanueldima@gmail.com>
2019-12-09 14:35:33 +0100

    content tree

--- webui/src/components/Resource.jsx
+++ webui/src/components/Resource.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Select from 'react-select';
 import { processMediatype } from '../actions/utils';
+import SortableTree from 'react-sortable-tree';
+import FileExplorerTheme from 'react-sortable-tree-theme-full-node-drag';
+import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 
 const SelectLanguage = (props) => {
     const value = props.languages.find(x => x.value == props.res.profile.language);
@@ -37,6 +40,53 @@ export class Resource extends React.Component {
         this.props.updateResource(newresource);
     }
 
+    renderTextContents() {
+        return (
+            <pre style={{whiteSpace: 'pre-wrap'}}>
+            ADVENTURES OF SHERLOCK HOLMES
+            Adventure I
+            A SCANDAL IN BOHEMIA
+            I
+            To Sherlock Holmes she is always _the_ woman. I have seldom heard
+            him mention her under any other name. In his eyes she eclipses and
+            predominates the whole of her sex. It was not that he felt any emotion
+            akin to love for Irene Adler. All emotions, and that one particularly,
+            were abhorrent to his cold, precise, but admirably balanced mind. He
+            was, I take it, the most perfect reasoning and observing machine that
+            the world has seen; but, as a lover, he would have placed himself in a
+            false position. He never spoke of the softer passions, save with a gibe
+            and a sneer.
+            </pre>
+        );
+    }
+
+    renderZipContents() {
+        return (
+            <Tree/>
+        );
+    }
+
+    renderContents() {
+        return (
+            <div className="row" style={{marginTop:10}}>
+                <div className="col-md-4">
+                </div>
+                <div className="col-md-8">
+                    <div className="welll">
+                        <div className="resource-header">
+                            <span className="glyphicon glyphicon-chevron-down" aria-hidden="true" style={{fontSize:'75%'}}/>Contents
+                        </div>
+                    </div>
+                    {
+                        // this.renderTextContents()
+                        this.renderZipContents()
+                    }
+
+                </div>
+            </div>
+        );
+    }
+
     render() {
         const res = this.props.resource;
         return <React.Fragment>
@@ -62,38 +112,88 @@ export class Resource extends React.Component {
                                 onLanguage={v => this.onChange('language', v)}/>
                         </div>
                     </div>
-                </div>
-                <div className="row" style={{marginTop:10}}>
-                    <div className="col-md-4">
-                    </div>
-                    <div className="col-md-8">
-                        <div className="welll">
-                        <div className="resource-header">
-                            <span className="glyphicon glyphicon-chevron-down" aria-hidden="true" style={{fontSize:'75%'}}/>Contents
-                        </div>
-
-                        <pre style={{whiteSpace: 'pre-wrap'}}>
-                        ADVENTURES OF SHERLOCK HOLMES
-Adventure I
-
-A SCANDAL IN BOHEMIA
-
-I
-
-To Sherlock Holmes she is always _the_ woman. I have seldom heard
-him mention her under any other name. In his eyes she eclipses and
-predominates the whole of her sex. It was not that he felt any emotion
-akin to love for Irene Adler. All emotions, and that one particularly,
-were abhorrent to his cold, precise, but admirably balanced mind. He
-was, I take it, the most perfect reasoning and observing machine that
-the world has seen; but, as a lover, he would have placed himself in a
-false position. He never spoke of the softer passions, save with a gibe
-and a sneer.
-                        </pre>
-                        </div>
-                    </div>
+                    {
+                        // this.renderContents()
+                    }
                 </div>
             </div>
         </React.Fragment>;
+    }
+}
+
+
+/////////////////////////////
+
+
+export default class Tree extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const some=[{title: '', children: []}];
+
+        this.state = {
+            treeData: [
+                { title: 'copyright.txt', children: []},
+                { title: 'doc', children: [
+                    { title: 'Revalidation_SK.html', children:[]},
+                    { title: 'dtd', children:some},
+                    { title: 'german-sampa.txt', children:[]},
+                    { title: 'papers', children:some},
+                    { title: 'pardoc', children:[
+                        { title: 'BasBPFDokuPRO.pdf', children: []},
+                        { title: 'BasBPFDokuSYNENG.pdf', children: []},
+                        { title: 'BasBPFDokuSYNGER.pdf', children: []},
+                        { title: 'BasBPFDokuSYNJAP.pdf', children: []},
+                        { title: 'BasBPFDokuSYNOverview.pdf', children: []},
+                        { title: 'BasFormatsPHOeng.html', children: []},
+                        { title: 'BasFormatsSAPdeu.html', children: []},
+                        { title: 'BasFormatsSAPeng.html', children: []},
+                        { title: 'BasFormatsdeu.html', children: []},
+                        { title: 'BasFormatseng.html', children: []},
+                        { title: 'BasProsodie.html', children: []},
+                        { title: 'BasSAMPA', children: []},
+                        { title: 'BasSKGESDoc.pdf', children: []},
+                        { title: 'BasSKUSHDoc.pdf', children: []},
+                        { title: 'BasSKUSPDoc.pdf', children: []},
+                        { title: 'DialogactReport-226-98.ps', children: []},
+                    ]},
+                    { title: 'quicktime', children:some},
+                    { title: 'readme.doc', children:[]},
+                    { title: 'readme.ges', children:[]},
+                    { title: 'readme.mar', children:[]},
+                    { title: 'readme.par', children:[]},
+                    { title: 'readme.trl', children:[]},
+                    { title: 'readme.trp', children:[]},
+                    { title: 'readme.ush', children:[]},
+                    { title: 'readme.usm', children:[]},
+                    { title: 'readme.woz.german', children:[]},
+                    { title: 'session-statistics', children:[]},
+                    { title: 'sk_ger.lex', children:[]},
+                    { title: 'techdocs', children:some},
+                    { title: 'trl-coding', children:some},
+                    { title: 'webpages.zip', children:[]},
+                ]},
+                { title: 'readme.annot', children: []},
+                { title: 'readme.main', children: []},
+                { title: 'readme.main.german', children: []},
+                { title: 'readme.meta', children: []},
+                { title: 'readme.skm', children: []},
+                { title: 'readme.skm.german', children: []},
+            ],
+        }
+    }
+
+    render() {
+        return (
+          <div style={{ height: 400, overflow:'auto'}}>
+            <SortableTree
+              treeData={this.state.treeData}
+              onChange={treeData => this.setState({ treeData })}
+              theme={FileExplorerTheme}
+              rowHeight={30}
+              slideRegionSize={0}
+            />
+          </div>
+        );
     }
 }
--- webui/webpack.config.js
+++ webui/webpack.config.js
@@ -21,11 +21,11 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 include: path.join(__dirname, 'src')
-            }
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'css-loader']
-            // },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
             // {
             //     test: /\.(png|woff|woff2|eot|ttf|svg)/,
             //     loader: 'url-loader?limit=100000'
